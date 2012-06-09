2fff5d4514ce2994c3c1e2e129ec6f552733f0cc
cedric.gourlay <cedric.gourlay@gmail.com>
2012-06-09 13:13:54 +0200

    handle request on context path /fridge or /fridge/ withoout id

--- src/main/java/com/agourlay/pomf/controller/FridgeController.java
+++ src/main/java/com/agourlay/pomf/controller/FridgeController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.agourlay.pomf.model.Fridge;
+import com.google.common.base.Strings;
 
 public class FridgeController extends HttpServlet{
 
@@ -16,10 +17,21 @@ public class FridgeController extends HttpServlet{
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException,ServletException {
 		//TODO add some string validation there
-		String fridgeId= req.getPathInfo().substring(1);
+		String fridgeId=req.getPathInfo();
+		validFridgeIdOrGoHome(req, resp, fridgeId);
+		fridgeId=fridgeId.substring(1);
+		validFridgeIdOrGoHome(req, resp, fridgeId);
 		Fridge fridge = Fridge.getOrCreateFridge(fridgeId);
 		req.setAttribute("fridgeId",fridge.getName());
 		RequestDispatcher rd = getServletContext().getRequestDispatcher("/fridge.jsp");
 		rd.forward(req, resp);
+	}
+
+	private void validFridgeIdOrGoHome(HttpServletRequest req, HttpServletResponse resp,
+			String fridgeId) throws ServletException, IOException {
+			if(Strings.isNullOrEmpty(fridgeId)){
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/");
+				rd.forward(req, resp);
+			}
 	}
 }
