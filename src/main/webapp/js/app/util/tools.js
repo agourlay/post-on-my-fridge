4f735b5df695f25d56331d435df7c551e273caa4
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2012-09-08 17:28:37 +0200

    refactor konami :D

--- src/main/webapp/js/app/util/tools.js
+++ src/main/webapp/js/app/util/tools.js
@@ -294,3 +294,31 @@ function filterData(data) {
 	data = data.replace(/<script.*\/>/, '');
 	return data;
 }
+
+function konami(){
+    $(window).konami(function(){
+        jQuery('.post').addClass('barrel_roll');
+        setTimeout(function(){
+            jQuery('.post').removeClass('barrel_roll');
+        },4000);  
+    }); 
+}
+
+(function() {
+	  (function($) {
+	    return $.fn.konami = function(callback) {
+	      var code, keysDown;
+	      code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+	      keysDown = [];
+	      return $(this).bind('keydown.konami', function(e) {
+	        keysDown.push(e.keyCode);
+	        if (keysDown.join('') !== code.slice(0, keysDown.length).join('')) {
+	          keysDown = [];
+	        }
+	        if (keysDown.length === code.length) {
+	          return callback();
+	        }
+	      });
+	    }
+	  })(jQuery);
+	}).call(this);
