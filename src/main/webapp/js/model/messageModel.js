932bc1b849ac80169f06c008fb26765ad66d3726
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2013-02-07 12:22:53 +0100

    setup branch

--- src/main/webapp/js/model/messageModel.js
+++
@@ -1,8 +0,0 @@
-App.Message = Em.Object.extend({
-	user: null,
-	message: null,
-	timestamp: null,
-	date: function (){
-		return moment(new Date(this.get('timestamp'))).format('HH:mm');
-	}.property()
-});
--- src/main/webapp/js/view/messageView.js
+++
@@ -1,9 +0,0 @@
-App.MessageView = Em.View.extend({
-	tagName: 'span',
-    templateName: 'message-template',
-	didInsertElement: function() {
-		$("#chatLog").animate({
-			scrollTop: $("#chatLog").prop("scrollHeight")
-		}, 2000);
-	}
-});
