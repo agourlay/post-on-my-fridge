635aa78c9434485056fae3a7624ffa13e13270d0
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2013-03-13 22:30:49 +0100

    adapt analytics for ember

--- pomf-client/js/app.js
+++ pomf-client/js/app.js
@@ -2,3 +2,17 @@ window.App = Ember.Application.createWithMixins({
 	LOG_TRANSITIONS: true,
 	LOG_BINDINGS : true
 });
+
+App.ApplicationController = Ember.Controller.extend({
+  currentPathChanged: function() {
+    var page;
+
+    // Track the page in Google Analytics
+    if (!Ember.isNone(_gaq)) {
+      page = window.location.hash.length > 0 ?
+             window.location.hash.substring(1) :
+             window.location.pathname;
+      _gaq.push(['_trackPageview', page]);
+    }
+  }.observes('currentPath')
+});
