bb8f0aa5cb1c871398ecb788e79fad5ab655785d
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2013-03-18 21:20:20 +0100

    quick and dirty xss filter bis

---
+++ pomf-api/src/main/scala/pomf/util/XssFilter.scala
@@ -0,0 +1,21 @@
+package pomf.util
+
+object XssFilter {
+  
+  val patterns:List[String] = List("/>",
+		  						  "</script>",
+		  						  "javascript:",
+		  						  "vbscript:",
+		  						  "<META",
+		  						  "<object",
+		  						  "onmouseover",
+		  						  "onblur",
+		  						  "onfocus",
+		  						  "onclick",
+		  						  "onload")
+  
+  def containsScript(txt:String):Boolean = {
+    patterns.exists(txt.contains(_))
+  }
+
+}
