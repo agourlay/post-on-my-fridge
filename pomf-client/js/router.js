39880dc85c2d040eb425ba97003c911bc86ff309
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2013-03-31 12:42:25 +0300

    fix router index path

--- pomf-client/js/router.js
+++ pomf-client/js/router.js
@@ -1,6 +1,6 @@
 // 'http://localhost:8888/#/fridge/demo
 App.Router.map(function() {
-    this.resource('index'); 
+    this.resource('index', { path:'/'}); 
     this.resource('fridges');
     this.resource('fridge', { path:'/fridge/:fridge_name' });
 });
