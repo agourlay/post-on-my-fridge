6a759397d03240a455cd736102eecf281cecffb9
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2013-05-20 20:27:29 +0200

    init dev

--- pomf-client/js/view/postsView.js
+++ pomf-client/js/view/postsView.js
@@ -9,24 +9,30 @@ App.PostsView = Ember.CollectionView.extend({
 		view.$().droppable({accept: ".post"});
 		konami();
 		
-		$("#search").autocomplete({
-			source : "api/search/fridge/",
-			delay : 100,
-			minLength : 2,
-			select : function(event, ui) {
-				view.get('controller').get('target').transitionToRoute('fridge', App.Dao.initSessionData(ui.item.value));
-			},
-			open: function (event, ui) {
-		        $('.ui-autocomplete').css('z-index', '99999');
-		    },
-			response : function(event, ui) {
-				if (ui.content.length === 0) {
-					ui.content.push({
-						label : "Click to create",
-						value : $("#search").val()
-					});
-				}
-			}
-		});
+		// $("#search").autocomplete({
+		// 	source : "api/search/fridge/",
+		// 	delay : 100,
+		// 	minLength : 2,
+		// 	select : function(event, ui) {
+		// 		view.get('controller').get('target').transitionToRoute('fridge', App.Dao.initSessionData(ui.item.value));
+		// 	},
+		// 	open: function (event, ui) {
+		//         $('.ui-autocomplete').css('z-index', '99999');
+		//     },
+		// 	response : function(event, ui) {
+		// 		if (ui.content.length === 0) {
+		// 			ui.content.push({
+		// 				label : "Click to create",
+		// 				value : $("#search").val()
+		// 			});
+		// 		}
+		// 	}
+		// });
+
+		$('#search').typeahead({
+		    name: 'fridges',
+		    minLength : 2,
+		    remote: 'api/search/fridge/?term=%QUERY'
+		  });
 	}
 });
