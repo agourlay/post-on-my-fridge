8c4437f7aea4fef080e61a60a8f472ea0a9e5e86
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2013-06-18 08:13:37 -0400

    add supervisor strategy

--- pomf-api/src/main/scala/pomf/Boot.scala
+++ pomf-api/src/main/scala/pomf/Boot.scala
@@ -3,6 +3,9 @@ package pomf
 import akka.actor._
 import akka.routing._
 import akka.io.IO
+import akka.actor.OneForOneStrategy
+import akka.actor.SupervisorStrategy._
+import scala.concurrent.duration._
 import spray.can.Http
 import akka.actor.actorRef2Scala
 import pomf.api.ApiHttpActor
@@ -15,8 +18,16 @@ object Boot extends App {
 
   // create and start our services actor
   val service = system.actorOf(Props[ApiHttpActor], "http-service")
-
-  val crudRouter = system.actorOf(Props[CrudServiceActor].withRouter(SmallestMailboxRouter(nrOfInstances = 1)), "crud-router")
+   
+  val supervisorStrategy =
+    OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1 minute) {
+      case _: NullPointerException     ⇒ Restart
+      case _: IllegalArgumentException ⇒ Stop
+      case _: Exception                ⇒ Escalate
+    }
+   
+  val crudRouter = system.actorOf(Props[CrudServiceActor]
+                         .withRouter(SmallestMailboxRouter(nrOfInstances = 1, supervisorStrategy = supervisorStrategy)), "crud-router")
   
   val notificationRouter = system.actorOf(Props[NotificationActor].withRouter(SmallestMailboxRouter(nrOfInstances = 1)), "notification-router")
     
