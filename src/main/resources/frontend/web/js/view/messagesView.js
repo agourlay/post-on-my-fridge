fc50ca42c39fa74cf1c8b6a91ace8c5c9330aaf4
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2013-10-18 21:10:29 +0200

    fix chat log size

--- src/main/resources/frontend/web/js/view/messagesView.js
+++ src/main/resources/frontend/web/js/view/messagesView.js
@@ -2,5 +2,17 @@ App.MessagesView = Ember.CollectionView.extend({
 	tagName: 'div',
 	elementId: 'chat-messages',
 	itemViewClass: 'App.MessageView',
-	contentBinding: 'controller.content'
+	contentBinding: 'controller.content',
+	
+	didInsertElement: function() {
+		var view = this;
+		view.fitSize();
+		$(window).resize(function() {
+  			view.fitSize();
+		});
+	},
+
+	fitSize : function() {
+		this.$().height($(window).height() - 185);
+	}
 });
