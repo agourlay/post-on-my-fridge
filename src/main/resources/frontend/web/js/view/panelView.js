df09eb5d288058ecd0963135ca53e2942d1261e7
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2013-10-27 20:53:40 +0100

    implement participants counter

--- src/main/resources/frontend/web/js/view/panelView.js
+++ src/main/resources/frontend/web/js/view/panelView.js
@@ -24,6 +24,22 @@ App.PanelView = Ember.View.extend({
 
 	didInsertElement : function() {	
 		var view = this;
+
+		$.getJSON("chat/" + App.Dao.get('fridgeId') + "/participants", function(number) {
+			if (number !== null) {
+				view.set("viewers", number);
+			}
+		});
+
+		App.Dao.get("eventBus").onValue(function(evt){     
+			if (evt.command === "participantAdded") {
+				view.set("viewers", view.get("viewers") + 1 );
+			}
+			if (evt.command === "participantRemoved") {
+				view.set("viewers", view.get("viewers") - 1 );
+			}
+        });
+
 		if(typeof store.get('username') !== "undefined"){
 			$("#pseudo").val(store.get('username'));
 		}
