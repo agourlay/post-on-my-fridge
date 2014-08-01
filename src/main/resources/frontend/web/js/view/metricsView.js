01b4f517b18a06e6f84117887c2e268e92d9ceb4
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2014-08-01 15:31:45 +0200

    various fixes

--- src/main/resources/frontend/web/js/view/metricsView.js
+++ src/main/resources/frontend/web/js/view/metricsView.js
@@ -8,6 +8,15 @@ App.MetricsView = Em.View.extend({
 	}	      
 });
 
+Handlebars.registerHelper('duration', function(number, digits) {
+	var value = Ember.get(this,number);
+	if (value > 1000) {
+		return (value / 1000).toFixed(digits) + " s";
+	} else {
+		return value.toFixed(digits) + " ms";
+	}	
+});
+
 Handlebars.registerHelper('toFixed', function(number, digits) {
   return Ember.get(this,number).toFixed(digits);
 });
